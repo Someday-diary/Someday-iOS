b2d79a12d85c44c536420cd27ead174eb19c63e0
Skyline-23 <bs2740@naver.com>
2021-09-24 21:14:09 +0900

    [UPDATE] add buttons to scroll view

--- diary/Source/View/SideMenu/SieMenuScrollView.swift
+++ diary/Source/View/SideMenu/SieMenuScrollView.swift
@@ -11,7 +11,8 @@ final class SideMenuScrollView: UIView {
     
     // MARK: - Constants
     fileprivate struct Metric {
-        
+        static let contentArea = 40.f
+        static let buttonTop = 25.f
     }
     
     fileprivate struct Font {
@@ -30,10 +31,36 @@ final class SideMenuScrollView: UIView {
         $0.locations = [0, 0.1, 0.9, 1]
     }
     
+    let themeButton = DiarySideMenuListButton().then {
+        $0.icon.image = R.image.themeIcon()
+        $0.label.text = "테마 설정"
+    }
+    
+    let alarmButton = DiarySideMenuListButton().then {
+        $0.icon.image = R.image.alarmIcon()
+        $0.label.text = "일기 알람 설정"
+    }
+    
+    let lockButton = DiarySideMenuListButton().then {
+        $0.icon.image = R.image.lockIcon()
+        $0.label.text = "잠금 설정"
+    }
+    
+    let infoButton = DiarySideMenuListButton().then {
+        $0.icon.image = R.image.openSourceIcon()
+        $0.label.text = "앱 정보"
+    }
+    
+    let feedbackButton = DiarySideMenuListButton().then {
+        $0.icon.image = R.image.feedbackIcon()
+        $0.label.text = "사용자 피드백"
+    }
+    
     // MARK: - Inititalizing
     override init(frame: CGRect) {
         super.init(frame: .zero)
         
+        self.scrollView.showsVerticalScrollIndicator = false
     }
     
     required init?(coder: NSCoder) {
@@ -52,6 +79,13 @@ final class SideMenuScrollView: UIView {
         self.addSubview(self.scrollView)
         self.scrollView.addSubview(self.contentView)
         
+        // Buttons
+        self.contentView.addSubview(self.themeButton)
+        self.contentView.addSubview(self.alarmButton)
+        self.contentView.addSubview(self.lockButton)
+        self.contentView.addSubview(self.infoButton)
+        self.contentView.addSubview(self.feedbackButton)
+        
         
         self.scrollView.snp.makeConstraints {
             $0.edges.equalToSuperview()
@@ -60,7 +94,32 @@ final class SideMenuScrollView: UIView {
         self.contentView.snp.makeConstraints {
             $0.edges.equalToSuperview()
             $0.width.equalToSuperview()
-            $0.height.equalTo(1200)
+        }
+        
+        self.themeButton.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalToSuperview().offset(Metric.contentArea)
+        }
+        
+        self.alarmButton.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalTo(self.themeButton.snp.bottom).offset(Metric.buttonTop)
+        }
+        
+        self.lockButton.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalTo(self.alarmButton.snp.bottom).offset(Metric.buttonTop)
+        }
+        
+        self.infoButton.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalTo(self.lockButton.snp.bottom).offset(Metric.buttonTop)
+        }
+        
+        self.feedbackButton.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalTo(self.infoButton.snp.bottom).offset(Metric.buttonTop)
+            $0.bottom.equalToSuperview().offset(-Metric.contentArea)
         }
         
     }
