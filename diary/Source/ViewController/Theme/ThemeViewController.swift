a823aed44992ca8594b50cd895d0cee6d29c0cf6
Skyline-23 <bs2740@naver.com>
2021-10-06 23:50:31 +0900

    [ADD] Add ThemeViewController

---
+++ diary/Source/ViewController/Theme/ThemeViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ThemeViewController.swift
+//  diary
+//
+//  Created by 김부성 on 2021/10/06.
+//
+
+import UIKit
+
+import ReactorKit
+
+final class ThemeViewController: BaseViewController, View {
+    
+    typealias Reactor = ThemeViewReactor
+    
+    // MARK: - Constants
+    fileprivate struct Metric {
+        
+    }
+    
+    fileprivate struct Font {
+        
+    }
+    
+    // MARK: - Properties
+    
+    // MARK: - UI
+    
+    // MARK: - Inintializing
+    init(reactor: Reactor) {
+        super.init()
+        defer {
+            self.reactor = reactor
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+    }
+    
+    override func setupConstraints() {
+        super.setupConstraints()
+        
+    }
+    
+    // MARK: - Configuring
+    func bind(reactor: ThemeViewReactor) {
+        
+    }
+}
