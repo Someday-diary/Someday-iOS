251de1dab0ce6bd96e51fc3036aa8919bc46c055
Skyline-23 <bs2740@naver.com>
2021-10-13 13:25:23 +0900

    [UPDATE] update ThemeViewReactor

--- diary/Source/ViewController/Theme/ThemeViewReactor.swift
+++ diary/Source/ViewController/Theme/ThemeViewReactor.swift
@@ -17,26 +17,69 @@ final class ThemeViewReactor: Reactor, Stepper {
     let initialState: State
     
     enum Action {
-        
+        case refresh
+        case selected(IndexPath)
     }
     
     enum Mutation {
-        
+        case refreshCells([UIImage], [String])
+        case updateAppearance(Int)
     }
     
     struct State {
+        let appearanceTitles: [String] = ["시스템 설정 모드", "라이트 모드", "다크 모드"]
+        let appearanceImages: [UIImage] = [R.image.systemMode()!, R.image.lightMode()!, R.image.darkMode()!]
+        var appearanceSelected: Int
         
+        var appearanceSectionItems: [ThemeViewSectionItem] = []
+        var sections: [ThemeViewSection] {
+            let section: [ThemeViewSection] = [
+                .appearance(self.appearanceSectionItems)
+            ]
+            return section
+        }
     }
     
     init() {
-        self.initialState = State()
+        self.initialState = State(appearanceSelected: UserDefaults.standard.integer(forKey: "Appearance"))
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
-        
+        switch action {
+        case .refresh:
+            return Observable.just(Mutation.refreshCells(currentState.appearanceImages, currentState.appearanceTitles))
+            
+        case let .selected(indexPath):
+            switch indexPath.section {
+            case 0:
+                return Observable.concat([
+                    Observable.just(Mutation.updateAppearance(indexPath.row)),
+                    Observable.just(Mutation.refreshCells(currentState.appearanceImages, currentState.appearanceTitles))
+                ])
+            default:
+                return Observable.empty()
+            }
+        }
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
         
+        switch mutation {
+        case let .refreshCells(image, title):
+            state.appearanceSectionItems.removeAll()
+            
+            for idx in 0..<title.count {
+                state.appearanceSectionItems.append(
+                    .appearance(AppearanceReactor(model: AppearnceModel(image: image[idx], title: title[idx], isSelected: idx == self.currentState.appearanceSelected)))
+                )
+            }
+            
+        case let .updateAppearance(idx):
+            state.appearanceSelected = idx
+            UserDefaults.standard.set(idx, forKey: "Appearance")
+        }
+        
+        return state
     }
 }
