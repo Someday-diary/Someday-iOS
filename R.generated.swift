7ce273c78d13a02e4d0f02669f847dc1bd077beb
Skyline-23 <bs2740@naver.com>
2021-10-20 15:48:58 +0900

    [ADD] add ThemeModel

--- R.generated.swift
+++ R.generated.swift
@@ -434,6 +434,20 @@ struct R: Rswift.Validatable {
     fileprivate init() {}
   }
 
+  /// This `R.file` struct is generated, and contains static references to 1 files.
+  struct file {
+    /// Resource file `ThemeCell`.
+    static let themeCell = Rswift.FileResource(bundle: R.hostingBundle, name: "ThemeCell", pathExtension: "")
+
+    /// `bundle.url(forResource: "ThemeCell", withExtension: "")`
+    static func themeCell(_: Void = ()) -> Foundation.URL? {
+      let fileResource = R.file.themeCell
+      return fileResource.bundle.url(forResource: fileResource)
+    }
+
+    fileprivate init() {}
+  }
+
   /// This `R.image` struct is generated, and contains static references to 23 images.
   struct image {
     /// Image `Add`.
---
+++ diary/Source/Model/Theme.swift
@@ -0,0 +1,22 @@
+//
+//  Theme.swift
+//  diary
+//
+//  Created by 김부성 on 2021/10/20.
+//
+
+import UIKit
+
+struct ThemeModel: ModelType {
+    var firstColor: UIColor
+    var secondColor: UIColor
+    var thirdColor: UIColor
+    var isSelected: Bool
+    
+    init(firstColor: UIColor, secondColor: UIColor, thirdColor: UIColor, isSelected: Bool) {
+        self.firstColor = firstColor
+        self.secondColor = secondColor
+        self.thirdColor = thirdColor
+        self.isSelected = isSelected
+    }
+}
