398c62f5e233511c4521717135a3d724b18b486e
Skyline-23 <bs2740@naver.com>
2021-12-28 22:10:01 +0900

    [UPDATE] update SideMenuViewReactor to show UserFeedback

--- diary/Source/ViewController/SideMenu/SideMenuViewReactor.swift
+++ diary/Source/ViewController/SideMenu/SideMenuViewReactor.swift
@@ -89,13 +89,8 @@ final class SideMenuViewReactor: Reactor, Stepper {
             return Observable.empty()
         
         case .userFeedBack:
-            let email = "somedayteam2021@gmail.com".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-            let title = "오늘하루 유저 피드백".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-            let mailUrl = URL(string: "mailto:\(email)?subject=\(title)")!
-            
-            if UIApplication.shared.canOpenURL(mailUrl) {
-                UIApplication.shared.open(mailUrl, options: [:])
-            }
+            self.steps.accept(DiaryStep.dismiss)
+            self.steps.accept(DiaryStep.feedbackIsRequired)
             
             return Observable.empty()
             
